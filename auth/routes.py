df460fd508fcc2a25af64750676d1944035bd275
Matheus Boscariol <moboscariol2010@gmail.com>
2025-11-19 16:40:26 -0500

    Fixed token refresh and added dockerfile

--- auth/routes.py
+++ auth/routes.py
@@ -1,6 +1,9 @@
+#  SPDX-License-Identifier: AGPL-3.0-or-later
+
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
+from pydantic import BaseModel
 import jwt
 
 router = APIRouter()
@@ -11,6 +14,10 @@ REFRESH_SECRET_KEY = "ed8384a37d0a1b10a919a8e292c25f769c7e08f030de183963c1c90deb
 ALGORITHM = "HS256"
 AUTH_EXPIRY = 30#3600
 REFRESH_EXPIRY = 120#2592000
+
+class RefreshRequest(BaseModel):
+    access_token: str
+    refresh_token: str
 
 # --- Create Tokens ---
 def create_access_token(data: dict, expires_in: int = AUTH_EXPIRY):
@@ -28,7 +35,7 @@ def create_refresh_token(data: dict, expires_in: int = REFRESH_EXPIRY):
     return jwt.encode(to_encode, REFRESH_SECRET_KEY, algorithm=ALGORITHM)
 
 # --- Login endpoint ---
-@router.post("/api/v1/auth/login")
+@router.post("/login")
 def login(email: str, password: str):
     # validate credentials (temporary since thers no db yet)
     if email != "test@example.com" or password != "123":
@@ -41,7 +48,7 @@ def login(email: str, password: str):
     return {"access_token": token, "refresh_token": refresh_token, "token_type": "bearer"}
 
 # --- Dependency for protected routes ---
-def get_current_user(token: str = Depends(oauth2_scheme)): #Depends() unencrypts the token
+def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, AUTH_SECRET_KEY, algorithms=[ALGORITHM])
         user_id = payload.get("sub")
@@ -49,24 +56,27 @@ def get_current_user(token: str = Depends(oauth2_scheme)): #Depends() unencrypts
     except jwt.PyJWTError:
         raise HTTPException(status_code=401, detail="Invalid Token!")
     
-@router.post("/api/v1/auth/refresh_token")
-def refresh_token(auth_token: str, refresh_token: str): #Depends() unencrypts the token
-    auth_token = Depends(oauth2_scheme)
-    refresh_token = Depends(oauth2_scheme)
+@router.post("/refresh_token")
+def refresh_token(body: RefreshRequest = None):
+    auth_token = body.access_token
+    refresh_token = body.refresh_token
+
+    try:
+        payload2 = jwt.decode(auth_token, AUTH_SECRET_KEY, algorithms=[ALGORITHM])
+        auth_token_decoded = payload2.get("sub")
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid Auth Token!")
 
     try:
         payload = jwt.decode(refresh_token, REFRESH_SECRET_KEY, algorithms=[ALGORITHM])
         refresh_token_decoded = payload.get("sub")
     except jwt.PyJWTError:
         raise HTTPException(status_code=401, detail="Invalid Refresh Token!")
+
+    print(auth_token_decoded)
+    print(refresh_token_decoded)
     
-    try:
-        payload2 = jwt.decode(auth_token, AUTH_SECRET_KEY, algorithms=[ALGORITHM])
-        auth_token_decoded = payload2.get("sub")
-    except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid Auth Token!")
-    
-    if auth_token_decoded != "user123" or refresh_token_decoded != "xyz987": # TODO: Change this to check for match in the database between 2 tokens
+    if auth_token_decoded != "abc123" or refresh_token_decoded != "xyz987": # TODO: Change this to check for match in the database between 2 tokens
         raise HTTPException(status_code=401, detail="Invalid Credentials")
 
     new_auth = create_access_token({"sub": auth_token_decoded})
@@ -75,7 +85,7 @@ def refresh_token(auth_token: str, refresh_token: str): #Depends() unencrypts th
     return {"access_token": new_auth, "refresh_token": new_refresh}
 
 
-@router.get("/api/v1/profile")
+@router.get("/profile")
 def get_profile(user_id: str):
     user_id = get_current_user(user_id)
     return {"user_id": user_id, "message": "Protected Content"}
--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-'''
-
-    Momentum Fitness Server Backend
-    Copyright (C) 2025  Matheus De Oliveira Boscariol
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-'''
+#  SPDX-License-Identifier: AGPL-3.0-or-later
 
 from fastapi import FastAPI
 from auth.routes import router as auth_router
 
 app = FastAPI()
 
-app.include_router(auth_router, prefix="/auth")
+app.include_router(auth_router, prefix="/api/v1/auth")
