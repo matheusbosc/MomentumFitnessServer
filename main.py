f25444e39892ecc50eeaba0f1c7676e4b60efaaa
Matheus Boscariol <moboscariol2010@gmail.com>
2025-11-21 18:15:56 -0500

    a

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 from fastapi import FastAPI
 from auth.routes import router as auth_router
 
-app = FastAPI(docs_url="/api/docs", redoc_url="/api/redoc")
+app = FastAPI(docs_url="/docs", redoc_url="/redoc", root_path="/api")
 
 app.include_router(auth_router, prefix="/api/v1/auth")
