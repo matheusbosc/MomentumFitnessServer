b3101b39bfcd30f8a408266071f5ea74c29ef8db
Matheus Boscariol <moboscariol2010@gmail.com>
2025-11-24 21:39:05 -0500

    added registering and some optimizations

--- auth/routes.py
+++ auth/routes.py
@@ -5,10 +5,12 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy import select, delete
 from models import User, RefreshToken
 from db import get_db
 from passlib.context import CryptContext
 import uuid
+import re
 
 import jwt
 
@@ -22,10 +24,18 @@ REFRESH_SECRET_KEY = "ed8384a37d0a1b10a919a8e292c25f769c7e08f030de183963c1c90deb
 ALGORITHM = "HS256"
 AUTH_EXPIRY = 300#3600
 REFRESH_EXPIRY = 120#2592000
+EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 
 class RefreshRequest(BaseModel): 
     access_token: str 
     refresh_token: str
+
+class UserRequest(BaseModel):
+    username: str
+    email: str
+    firstname: str
+    lastname: str
+    password: str
 
 # region HELPER FUNCTIONS 
 
@@ -59,11 +69,11 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
 
         user: User = db.query(User).filter(User.username == user_id).first() # --- SELECT * FROM users WHERE email = ? LIMIT 1;
         if not user:
-            raise HTTPException(status_code=401, detail="Invalid Credentials")
+            raise HTTPException(status_code=401, detail="invalid Credentials")
 
         return user
     except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid Token!")
+        raise HTTPException(status_code=401, detail="invalid Token!")
 
 # endregion
 
@@ -74,26 +84,50 @@ def login(email: str, password: str, db: Session = Depends(get_db)):
     
     # 1. Get user
 
-    if "@" in email:
-        user: User = db.query(User).filter(User.email == email).first() # --- SELECT * FROM users WHERE email = ? LIMIT 1;
+    query_user: User
+
+    if re.fullmatch(EMAIL_REGEX, email):
+        query = (
+            select(User)
+            .where(User.email == email)
+            .limit(1)
+        ) # --- SELECT * FROM users WHERE email = ? LIMIT 1;
+
+        for user in db.scalars(query):
+            query_user = user
+            break
+
+        else:
+            raise HTTPException(status_code=401, detail="invalid Credentials")
 
     else:
-        user: User = db.query(User).filter(User.username == email).first() # --- SELECT * FROM users WHERE username = ? LIMIT 1;
+        query = (
+            select(User)
+            .where(User.username == email)
+            .limit(1)
+        ) # --- SELECT * FROM users WHERE username = ? LIMIT 1;
 
-    if not user:
-            raise HTTPException(status_code=401, detail="Invalid Credentials")
-        
-    if not verify_password(password, user.password):
-        raise HTTPException(status_code=401, detail="Invalid Credentials!")
+        for user in db.scalars(query):
+            query_user = user
+            break
+
+        else:
+            raise HTTPException(status_code=401, detail="invalid Credentials")
     
+    if not verify_password(password, query_user.password):
+        raise HTTPException(status_code=401, detail="invalid Credentials!")
+
     # 2. Create IDs
     refresh_id_uuid = uuid.uuid4()
     refresh_id = str(refresh_id_uuid)
-    access_token = create_access_token({"sub": user.username})
+    access_token = create_access_token({"sub": query_user.username})
     refresh_token = create_refresh_token({"sub": refresh_id})
 
     # 3. Store the refresh token ID in DB
-    new_token = RefreshToken(user_id=user.username, refresh_id=refresh_id)
+    db.execute(
+        delete(RefreshToken).where(RefreshToken.user_id == query_user.username)
+    )
+    new_token = RefreshToken(user_id=query_user.username, refresh_id=refresh_id)
     db.add(new_token)
     db.commit()
 
@@ -102,6 +136,56 @@ def login(email: str, password: str, db: Session = Depends(get_db)):
         "refresh_token": refresh_token,
         "token_type": "bearer",
     }
+
+@router.post("/register")
+def register(body: UserRequest, db: Session = Depends(get_db)):
+    # 1. Verify if all data is filled correctly
+    if not re.fullmatch(EMAIL_REGEX, body.email):
+        raise HTTPException(status_code=400, detail="invalid email")
+    
+    if len(body.username) > 12 or len(body.username) < 3:
+        raise HTTPException(status_code=400, detail="username too long/short")
+    
+    if len(body.password) < 6:
+        raise HTTPException(status_code=400, detail="password too short")
+
+    # 2. Verify if user exists or doesnt exist
+
+    query = (
+        select(User)
+        .where(User.email == body.email)
+    )
+    
+    for user in db.scalars(query):
+        raise HTTPException(status_code=409, detail="email in use")
+    
+    query = (
+        select(User)
+        .where(User.username == body.username)    
+    )
+
+    for user in db.scalars(query):
+        raise HTTPException(status_code=409, detail="username in use")
+
+    # 3. Process and Commit Data
+
+    hashed_password = hash_password(body.password)
+    db_user = User(
+        email=body.email,
+        username=body.username,
+        password=hashed_password,
+        firstname=body.firstname,
+        lastname=body.lastname
+    )
+
+    db.add(db_user)
+    db.commit()
+
+    # 4. Login
+
+    login_response = login(email=body.email, password=body.password, db=db)
+
+    return login_response
 
 @router.post("/check_refresh_status")
 @router.post("/refresh_token")
@@ -112,23 +196,23 @@ def refresh_token(body: RefreshRequest, db: Session = Depends(get_db)):
         payload = jwt.decode(body.access_token, AUTH_SECRET_KEY, algorithms=[ALGORITHM])
         user_id = payload.get("sub")
     except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid Auth Token")
+        raise HTTPException(status_code=401, detail="invalid token")
 
     # Decode refresh token
     try:
         payload2 = jwt.decode(body.refresh_token, REFRESH_SECRET_KEY, algorithms=[ALGORITHM])
         refresh_id = payload2.get("sub")
     except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid Refresh Token")
+        raise HTTPException(status_code=401, detail="invalid token")
 
     # Look up in DB
-    stored = db.query(RefreshToken).filter(
+    stored: RefreshToken = db.query(RefreshToken).filter(
         RefreshToken.user_id == user_id,
         RefreshToken.refresh_id == refresh_id
     ).first()
 
     if not stored:
-        raise HTTPException(status_code=401, detail="Token Mismatch")
+        raise HTTPException(status_code=401, detail="token mismatch")
 
     # Issue new tokens
     new_access = create_access_token({"sub": user_id})
@@ -138,6 +222,9 @@ def refresh_token(body: RefreshRequest, db: Session = Depends(get_db)):
     new_refresh_token = create_refresh_token({"sub": new_refresh_id})
 
     # Update DB
+    db.execute(
+        delete(RefreshToken).where(RefreshToken.id == stored.id)
+    )
     stored.refresh_id = new_refresh_id
     db.commit()
 
